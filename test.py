264cd0d4c4738790137cc435cf7b901ec392577f
Mike Chen <mmiikkeea@gmail.com>
2015-08-04 11:11:34 +0800

    0804 commit 11:18 recover

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-print 100+200
+import math
+def move(x,y,step,angle=0):
+	nx=x+step*math.cos(angle)
+	ny=y-step*math.sin(angle)
+	return nx,ny
+x,y=move(100,100,60,math.pi/6)
+print(x,y)
 
-print "so"
-
-name = input()
-
-print name
+print 'second tme'
